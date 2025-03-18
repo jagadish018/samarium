5fb7e30021a7b836559eaed742a128d89d541260
jagadishck018 <jagadishck.018@gmail.com>
2025-03-18 10:31:50 +0530

    initialize project with Hono server and basic setup

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+serve(app, (info) => {
+  console.log("server is running is successfully");
+});
